0f65d664a55a5261b5a6290583bdc1345271c8c4
kimestalike <lavrovasd@mail.ru>
2025-03-20 23:48:55 +0400

    add missing tests

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -16,4 +16,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void shouldFindMaxWhenItsNotFirstMonth() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 12, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
